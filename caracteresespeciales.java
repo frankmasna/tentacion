f3caf1e788ba476c56e07cab36a0e13c4c250427
frank <202213859@utea.edu.pe>
2024-01-15 11:10:25 -0500

    otro confit

---
+++ caracteresespeciales.java
@@ -0,0 +1,8 @@
+public class caracteresespeciales {
+        public static void main(String[] args) {
+           System.out.println("una corchea: \u266A");    
+           System.out.print("Dos corcheas: \u266B");
+        }
+    }
+    
+
---
+++ salidaformateada2.java
@@ -0,0 +1,8 @@
+public class salidaformateada2 {
+    public static void main(String[] args) {
+        System.out.printf("El número %d no tiene decimales.\n", 32);
+        System.out.printf("El número %f sale con decimales.\n", 34.0);
+        System.out.printf("El %.3f sale exactamente con 3 decimales.\n", 28.0);
+       }
+   }
+
